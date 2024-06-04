126c2bfedbd2b15ef1500e13be583131497246c2
Vinul Vinsura <vinulviksura16@gmail.com>
2024-06-05 00:37:52 +0530

    creat login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./common/nav-bar/nav-bar.component";
+import { LoginComponent } from "./page/login/login.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavBarComponent]
+    imports: [RouterOutlet, NavBarComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'car-rental-app';
